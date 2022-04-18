93a293cd5997d4028c2383c754ff1825a8d7303a
waleedarshad <waleed_arshad006@hotmail.com>
2022-04-18 08:46:13 +0500

    Change theme color to green

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:git_demo_iba/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -50,15 +51,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _navigate() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ProfilePage()));
   }
 
   @override
@@ -106,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _navigate,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
