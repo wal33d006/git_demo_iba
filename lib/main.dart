2ca640c8ce323f57d0d2c3fcfb1363bc1c4b3f70
waleedarshad <waleed_arshad006@hotmail.com>
2022-04-18 08:48:25 +0500

    Add helloWorld function to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -50,15 +50,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void helloWorld() {
+    print('Hello');
   }
 
   @override
@@ -106,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: helloWorld,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
